686e7bc73f393e783f66fbebd6a344cf4f5d127f
kkxxss <65663880@qq.com>
2018-11-19 16:16:20 +0800

    更新报告生成方式

--- run_all.py
+++ run_all.py
@@ -28,24 +28,25 @@ def add_case(casepath=case_path, rule="test*.py"):
 
 def run_case(all_case, reportpath=report_path):
     '''执行所有的用例, 并把结果写入测试报告'''
-    # htmlreport = reportpath+r"\result.html"
-    # print("测试报告生成地址：%s"% htmlreport)
-    # fp = open(htmlreport, "wb")
-    # runner = HTMLTestRunner.HTMLTestRunner(stream=fp,
-    #                                            verbosity=2,
-    #                                            title="测试报告",
-    #                                            description="用例执行情况")
+    htmlreport = reportpath+r"\result.html"
+    # htmlreport = reportpath
+    print("测试报告生成地址：%s"% htmlreport)
+    fp = open(htmlreport, "wb")
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp,
+                                               verbosity=2,
+                                               title="测试报告",
+                                               description="用例执行情况")
     # runner = HTMLTestRunner.HTMLTestRunner(stream=fp,
     #                                        verbosity=2,
     #                                        title="测试报告",
     #                                        description="用例执行情况")
 
-    result = BeautifulReport.BeautifulReport(cases)
-    result.report(filename='测试报告', description='测试deafult报告', log_path='report')
+    # result = BeautifulReport.BeautifulReport(cases)
+    # result.report(filename=r'测试报告', descrition='测试deafult报告', log_path=u'report')
 
     # 调用add_case函数返回值
-    # result.run(all_case)
-    # fp.close()
+    runner.run(all_case)
+    fp.close()
 
 if __name__ == "__main__":
     cases = add_case()
